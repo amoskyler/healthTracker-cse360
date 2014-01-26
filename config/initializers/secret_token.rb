a8a74b4afb48cc061e47b9e713dc3627e5fdd8a4
Brandon Anderson <Brandon753.ba@gmail.com>
2014-01-25 23:43:26 -0700

    Initial Commit of a fresh Rails build.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HealthTracker::Application.config.secret_key_base = '60a51ad0d8bca682052920b1f621fca8f42529caf75b22eb628542045c430e198257315188dad550f5df4ead8602b814475cb14e0758e986166a79e0bedca537'
